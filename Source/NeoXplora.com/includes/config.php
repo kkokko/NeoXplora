90daa05dced95cdb57023b6ad58bccb312690fad
Jigisha <galoxsoft@gmail.com>
2014-08-01 18:28:20 +0530

    Modify QREP Search

--- Source/NeoXplora.com/includes/config.php
+++ Source/NeoXplora.com/includes/config.php
@@ -8,6 +8,7 @@ define("DBU", "userneo123");
 define("DBPASS", "edu3uvy4e");
 define("DB", "zadmin_neo123");
 
+
 $link = mysql_connect(HOST, DBU, DBPASS);
 $db = mysql_select_db(DB, $link);
 
@@ -35,11 +36,7 @@ define('RIGHT_BOX', true);
 include ROOT . 'classes/UserSessionManager.php';
 include ROOT . 'classes/EmailAddressValidator.php';
 include ROOT . 'classes/UserUtils.php';
-<<<<<<< HEAD
-include(ROOT .'includes/db/dbSQLFunctions.php');
-=======
-include(ROOT .'includes/db/dbSQLFunctions.php'); 
->>>>>>> origin/master
+
 require_once( ROOT . 'blog/wp-load.php' );
 UserSessionManager::StartSession();
 
